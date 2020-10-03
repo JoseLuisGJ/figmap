14c556c27a3ee78a1b224be15f44483903ef1fde
joseluis.gonzalez <joseluisgj@gmail.com>
2020-10-03 09:56:46 +0200

    Plugin working with React

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,48 +1,98 @@
-import * as React from 'react';
-import '../styles/ui.css';
+import * as React from "react";
+import "../styles/ui.css";
 
 declare function require(path: string): any;
 
 const App = ({}) => {
-    const textbox = React.useRef<HTMLInputElement>(undefined);
+  const textbox = React.useRef<HTMLInputElement>(undefined);
+  let mapStyle;
+  let accessToken;
+  let username;
+  let style_id;
+  let overlay;
+  let lng = "-77.03968";
+  let lat = "38.89744";
+  let zoom = 9;
+  let bearing = 0;
+  let pitch = 0;
+  let auto;
+  let width = 800;
+  let height = 800;
+  let retina = true ? "@2x" : "";
+  let attribution = false;
+  let logo = false;
+  let map;
 
-    const countRef = React.useCallback((element: HTMLInputElement) => {
-        if (element) element.value = '5';
-        textbox.current = element;
-    }, []);
+  const onDrawMap = React.useCallback(() => {
+    // Retrive statica map from mapbox style https://docs.mapbox.com/api/maps/#retrieve-a-static-map-from-a-style
+    zoom = map.getZoom();
+    pitch = map.getPitch();
+    bearing = map.getBearing();
+    lng = map.getCenter().lng;
+    lat = map.getCenter().lat;
+    let imurl = `https://api.mapbox.com/styles/v1/${username}/${style_id}/static/${lng},${lat},${zoom},${bearing},${pitch}/${width}x${height}${retina}?access_token=${accessToken}`;
+    fetch(imurl)
+      .then(r => r.arrayBuffer())
+      .then(a =>
+        parent.postMessage(
+          { pluginMessage: { type: "draw-map", data: new Uint8Array(a) } },
+          "*"
+        )
+      );
+  }, []);
 
-    const onCreate = React.useCallback(() => {
-        const count = parseInt(textbox.current.value, 10);
-        parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
-    }, []);
+  const onGetMap = React.useCallback(() => {
+    // Get values from inputs
+    username = document.getElementById("usernameInput").value;
+    style_id = document.getElementById("mapStyleInput").value;
+    accessToken = document.getElementById("tokenInput").value;
+    // Draw Mapbox map
+    mapboxgl.accessToken = accessToken;
+    map = new mapboxgl.Map({
+      container: "map", // container id
+      style: `mapbox://styles/${username}/${style_id}`, // style URL
+      center: [lng, lat], // starting position [lng, lat]
+      zoom: zoom // starting zoom
+    });
+  }, []);
 
-    const onCancel = React.useCallback(() => {
-        parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
-    }, []);
+  React.useEffect(() => {
+    // This is how we read messages sent from the plugin controller
+    window.onmessage = event => {
+      const { type, message } = event.data.pluginMessage;
+      if (type === "create-rectangles") {
+        console.log(`Figma Says: ${message}`);
+      }
+    };
+  }, []);
 
-    React.useEffect(() => {
-        // This is how we read messages sent from the plugin controller
-        window.onmessage = (event) => {
-            const { type, message } = event.data.pluginMessage;
-            if (type === 'create-rectangles') {
-                console.log(`Figma Says: ${message}`);
-            };
-        }
-    }, []);
-
-    return (
-        <div>
-            <img src={require('../assets/logo.svg')} />
-            <h2>Rectangle Creator</h2>
-            <p>
-                Count: <input ref={countRef} />
-            </p>
-            <button id="create" onClick={onCreate}>
-                Create
-            </button>
-            <button onClick={onCancel}>Cancel</button>
-        </div>
-    );
+  return (
+    <div>
+      {/* <img src={require('../assets/logo.svg')} /> */}
+      <h2>Mapbox to Figma</h2>
+      <p>
+        Mapbox API access token:{" "}
+        <input
+          id="tokenInput"
+          value="pk.eyJ1IjoicWF0aXVtIiwiYSI6ImNrM2lrNzE1djA4a3ozY2xjeDFiMzA3b24ifQ.baOd_O4sWca3ma4klyW7Mw"
+        />
+      </p>
+      <p>
+        Mapbox user name: <input id="usernameInput" value="qatium" />
+      </p>
+      <p>
+        Mapbox Style ID:
+        <input id="mapStyleInput" value="ckaf3fzi6200k1ipufjkbt50v" />
+      </p>
+      <button id="get-map" onClick={onGetMap}>
+        Get Map from Mapbox
+      </button>
+      <button id="draw-map" onClick={onDrawMap}>
+        Draw map to Figma
+      </button>
+      <div id="map">Map here</div>
+    </div>
+  );
 };
 
 export default App;
--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,26 +1,42 @@
-figma.showUI(__html__);
+const pluginWidth = 800;
+const pluginHeight = 560;
 
-figma.ui.onmessage = (msg) => {
-    if (msg.type === 'create-rectangles') {
-        const nodes = [];
+figma.showUI(__html__, {
+  width: pluginWidth,
+  height: pluginHeight
+});
 
-        for (let i = 0; i < msg.count; i++) {
-            const rect = figma.createRectangle();
-            rect.x = i * 150;
-            rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-            figma.currentPage.appendChild(rect);
-            nodes.push(rect);
-        }
+figma.ui.onmessage = msg => {
+  if (msg.type === "create-rectangles") {
+    const nodes = [];
 
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
-
-        // This is how figma responds back to the ui
-        figma.ui.postMessage({
-            type: 'create-rectangles',
-            message: `Created ${msg.count} Rectangles`,
-        });
+    for (let i = 0; i < msg.count; i++) {
+      const rect = figma.createRectangle();
+      rect.x = i * 150;
+      rect.fills = [{ type: "SOLID", color: { r: 1, g: 0.5, b: 0 } }];
+      figma.currentPage.appendChild(rect);
+      nodes.push(rect);
     }
 
-    figma.closePlugin();
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+
+    // This is how figma responds back to the ui
+    figma.ui.postMessage({
+      type: "create-rectangles",
+      message: `Created ${msg.count} Rectangles`
+    });
+  }
+
+  if (msg.type === "draw-map") {
+    let imageHash = figma.createImage(msg.data).hash;
+    const rect = figma.createRectangle();
+    rect.fills = [{ type: "IMAGE", scaleMode: "FIT", imageHash }];
+    figma.currentPage.appendChild(rect);
+    // select the rectangle and focus the viewport
+    figma.currentPage.selection = [rect];
+    figma.viewport.scrollAndZoomIntoView([rect]);
+  }
+
+  figma.closePlugin();
 };
