b14a6986697a971981fae78c01feceb1dbe7937c
José Luis González <joseluisgj@gmail.com>
2024-07-07 00:20:13 +0200

    More Mapbox default styles

--- src/app/components/MapStylesForm.tsx
+++ src/app/components/MapStylesForm.tsx
@@ -102,14 +102,14 @@ const MapStylesForm: React.FC<IMap> = ({
             }}
             defaultValue={mapboxStyle}
           >
-            <option value="streets-v11">Streets</option>
-            <option value="light-v10">Light</option>
-            <option value="dark-v10">Dark</option>
-            <option value="outdoors-v11">Outdoors</option>
+            <option value="streets-v12">Streets</option>
+            <option value="outdoors-v12">Outdoors</option>
+            <option value="light-v11">Light</option>
+            <option value="dark-v11">Dark</option>
             <option value="satellite-v9">Satellite</option>
-            <option value="satellite-streets-v11">
-              Satellite with streets
-            </option>
+            <option value="satellite-streets-v12">Satellite Streets</option>
+            <option value="navigation-day-v1">Navigation Day</option>
+            <option value="navigation-night-v1">Navigation Night</option>
           </select>
         </div>
       )}
